375aaf9defad17519e09d480a22f2c21e8242314
isuranga <isuranga.official@gmail.com>
2025-11-28 19:43:14 +0800

    resolved errors

--- BERT_explainability/modules/BERT/BertForSequenceClassification.py
+++ BERT_explainability/modules/BERT/BertForSequenceClassification.py
@@ -1,4 +1,5 @@
 from transformers import BertPreTrainedModel
+from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers.utils import logging
 from BERT_explainability.modules.layers_ours import *
 from BERT_explainability.modules.BERT.BERT import BertModel
